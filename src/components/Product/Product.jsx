8b5e90f918af291b4b1ef84c7a3bc0af688a3c12
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-17 20:20:25 +0600

    refactor: Add handleAddToCart function to Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,6 +4,11 @@ import './Product.css';
 
 const Product = (props) => {
     const { name, img, seller, price, stock, ratings } = props.product;
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
+        
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,9 +18,9 @@ const Product = (props) => {
                 <p>Manufacturer:{seller}</p>
                 <p>Rating:{ratings}Star:</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() =>handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
-    );
+    ); 
 };
 
 export default Product;
