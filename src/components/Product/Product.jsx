f65962856b546bf301cad22f9dd3138b4e142344
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-18 21:33:49 +0600

    refactor: Update FontAwesomeIcon in Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -23,7 +23,7 @@ const Product = (props) => {
             </div>
             <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
                 Add to Cart
-                <FontAwesomeIcon icon={faShoppingCart} />
+                <FontAwesomeIcon icon={faShoppingCart} /> 
             </button>
         </div>
     );
