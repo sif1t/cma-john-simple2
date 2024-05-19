c88fb22e60857c85dedfbebfdee56896ea4b363b
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-19 19:01:35 +0600

    refactor: Add sticky position to Cart component and calculate grand total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,13 +5,26 @@ const Cart = (props) => {
     // const cart = props.cart; // option 1
     const { cart } = props; // option 2
     console.log(cart);
+
+
+    let totalPrice = 0; 
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax  = totalPrice*7/100
+
+   const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>order samary</h4>
             <p>Items Ordered:{cart.length}</p>
-            <p>Total Price: </p>
-            <p>Total Shipping:</p>
-            <h6>Grand Total:</h6>
+            <p>Total Price:${totalPrice} </p>
+            <p>Total Shipping:${totalShipping}</p>
+            <p>Tax:{tax.toFixed(2)}</p>
+            <h6>Grand Total:${grandTotal.toFixed(2)}</h6> 
+            
         </div>
     );
 };
