d37e24bb2a15f4c3e7bd7bdcd683d435cd7fab9b
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-06-29 21:51:43 +0600

    refactor: Update ReviewItem component styles and functionality

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart,faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ReviewItem = ({ product }) => {
     const { id, img, price, name, quantity } = product;
@@ -14,7 +16,9 @@ const ReviewItem = ({ product }) => {
                 <p>Order Quantity: <span className='orange-text'>{quantity}
                 </span></p> 
             </div>
-            <button>p</button>
+            <button className='btn-delete'>
+                <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
